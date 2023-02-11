fcfc9834fc53a20c8453fcec44bece03e7c39d09
kardashq <kardashq@yandex.ru>
2023-02-11 13:57:15 +0300

    ch: added comments admin.py

--- app/admin.py
+++ app/admin.py
@@ -5,21 +5,24 @@ from models import (Customer, Account,
 
 
 @admin.register(Customer)
-"""Register CustomerModel in admin"""
 class CustomerAdmin(admin.ModelAdmin):
+    """Register Customer Model in AdminPanel"""
     list_display = ['first_name', 'last_name']
 
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
+    """Register Account Model in AdminPanel"""
     list_display = ['balance', 'moneybox']
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """Register Category Model in AdminPanel"""
     list_display = ['title']
 
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
+    """Register Transaction Model in AdminPanel"""
     list_display = ['type_of_transaction', 'user', 'category', 'amount', 'date']
