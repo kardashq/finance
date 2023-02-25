69a7bbaaef9a237ae25c069bfe09f56d4191710b
kardashq <kardashq@yandex.ru>
2023-02-25 10:42:55 +0300

    upd: urls, settings

--- app/urls.py
+++ app/urls.py
@@ -9,9 +9,9 @@ router = DefaultRouter()
 router.register('action', ActionViewSet)
 
 urlpatterns = [
-    path('statistic/', TransactionsAPIView.as_view()),
     path('account/', AccountDetail.as_view()),
-    path('download/', ExportTransactionsView.as_view()),
+    path('statistic/', TransactionsAPIView.as_view()),
     path('statistic/period/', MonthlyStatsView.as_view()),  # example http://127.0.0.1:8000/api/stat/period/?month=2&year=2023&t=income
+    path('download/', ExportTransactionsView.as_view()),
     path('', include(router.urls))
 ]
